f5f37c2a63dfd5d26e1a36302aeca6c3374a708a
tbalint19 <tothbalint719@gmail.com>
2024-09-19 19:58:09 +0200

    folder structure done

--- executables/microservices/admin-api/src/index.ts
+++ executables/microservices/admin-api/src/index.ts
@@ -1,12 +1,16 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
-import { add } from 'demo'
-import type { AddResult } from 'demo'
+import { add } from '../../../../packages/demo/out'
+import type { AddResult } from '../../../../packages/demo/out'
+import { users } from '../../../../packages/models/out'
+import type { UserData } from '../../../../packages/models/out'
 
 const app = new Hono()
 
 app.get('/', (c) => {
   const result: AddResult = add(1, 2)
+  const col = users.username
+  const data: UserData = { age: 10 }
   return c.text('Hello Hono!')
 })
 
