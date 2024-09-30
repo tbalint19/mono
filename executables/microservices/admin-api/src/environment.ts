caa6b26a03651b3fd129dd9941bdcb2d4ef8dbc2
tbalint19 <tothbalint719@gmail.com>
2024-09-30 19:38:58 +0200

    before svelte lib

--- executables/microservices/admin-api/src/environment.ts
+++ executables/microservices/admin-api/src/environment.ts
@@ -4,8 +4,10 @@ const ENVIRONMENT = z.object({
   CLIENT_ID: z.string(),
   CLIENT_SECRET: z.string(),
   REDIRECT_URI: z.string(),
+
   JWT_SECRET: z.string(),
   JW_EXPIRATION: z.string(),
+  
   LOG_LEVEL: z.union([ z.literal("none"), z.literal("error"), z.literal("all") ]),
   LOGGER_MODE: z.union([ z.literal("local"), z.literal("remote"), z.literal("combined") ]),
   LOGGER_REMOTE_SOURCE: z.string(),
