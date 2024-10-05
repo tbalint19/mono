861f79326d4fc036ffcacdaa2d0b71ed8603ea47
tbalint19 <tothbalint719@gmail.com>
2024-10-05 22:55:17 +0200

    doc updated

--- executables/database/src/client.ts
+++ executables/database/src/client.ts
@@ -1,10 +1,20 @@
 import postgres from "postgres";
 
-const host = process.env.DATABASE_HOST
-const database = process.env.DATABASE_NAME
-const user = process.env.DATABASE_USER
-const password = process.env.DATABASE_PASSWORD
-const port = process.env.DATABASE_PORT
+const host = process.env.NODE_ENV === "test" ?
+  process.env.TEST_DATABASE_HOST :
+  process.env.DATABASE_HOST
+const database = process.env.NODE_ENV === "test" ?
+  process.env.TEST_DATABASE_NAME :
+  process.env.DATABASE_NAME
+const user = process.env.NODE_ENV === "test" ?
+  process.env.TEST_DATABASE_USER :
+  process.env.DATABASE_USER
+const password = process.env.NODE_ENV === "test" ?
+  process.env.TEST_DATABASE_PASSWORD :
+  process.env.DATABASE_PASSWORD
+const port = process.env.NODE_ENV === "test" ?
+  process.env.TEST_DATABASE_PORT :
+  process.env.DATABASE_PORT
 
 if (!host || !database || !user || !password)
   throw new Error("No connection string specified")
